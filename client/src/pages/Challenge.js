2d401adaccbfb202f2a5a6cc0163d5c3ce7a9f0d
moring-swe <moring.swe@gmail.com>
2022-05-26 15:45:08 +0200

    Layout

--- client/src/pages/Challenge.js
+++ client/src/pages/Challenge.js
@@ -11,10 +11,29 @@ const Challenge = () =>{
 
 function Square(props) {
 	return(
-		<button className = "square" onClick={props.onClick}>
+		<button 
+		className = {"square " + props.className}
+		onClick={props.onClick}
+		id = {props.id}
+		>
 		{props.value+" TEST"}
 		</button>
 	);
+}
+
+class Timer extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			remainTime: 20000,
+		}
+	}
+
+	render(){
+		return(
+			<canvas className = "timer" height='6px'/>
+			);
+	}
 }
 
 class Match extends React.Component{
@@ -23,6 +42,8 @@ class Match extends React.Component{
 		this.state = {
 			prompts: Array(4).fill(null),
 			answers: Array(4).fill(null),
+			choosePromptId: "",
+			chooseAnswerId: "",
 			userMatches: Array(4).fill([null,null]),
 			gameHeight: 0,
 			buttonYPoints: Array(4).fill(null),
@@ -33,8 +54,10 @@ class Match extends React.Component{
 		return (
 			<Square 
 			value={this.state.prompts[i]}
-			onClick={()=>{
-				console.log("!!!");
+			className = "prompt"
+			id = {"prompt" + i}
+			onClick={(event)=>{
+				this.setPromptColor(event)
 				this.setPrompts(["1","2","3","4"]);
 				}
 			}
@@ -46,12 +69,48 @@ class Match extends React.Component{
 		return (
 			<Square
 			value={this.state.answers[i]}
-			onClick={()=>{
-				// do something...
+			className = "answer"
+			id = {"answer" + i}
+			onClick={(event)=>{
+				this.setAnswerColor(event)
 			}} />
 			);
 	}
+
+	renderTimer(){
+		return(
+			<Timer id='timer' />
+			);
+	}
 	
+	setPromptColor(event){
+		this.setState(
+		{
+			choosePromptId: event.target.id,
+		});
+		var square = document.getElementById(event.target.id);
+		var squares = document.getElementsByClassName("prompt");
+		console.log(squares);
+		for(const s of squares){
+			s.style.backgroundColor = '';
+		}
+		square.style.backgroundColor = 'limegreen';
+	}
+
+	setAnswerColor(event){
+		this.setState(
+		{
+			chooseAnswerId: event.target.id,
+		});
+		var square = document.getElementById(event.target.id);
+		var squares = document.getElementsByClassName("answer");
+		console.log(squares);
+		for(const s of squares){
+			s.style.backgroundColor = '';
+		}
+		square.style.backgroundColor = 'limegreen';
+	}
+
 	setPrompts(promptsArray){
 		this.setState({
 			prompts: promptsArray,
@@ -83,7 +142,9 @@ class Match extends React.Component{
 					{this.renderPrompt(2)}
 					{this.renderPrompt(3)}
 					</div>
-					<canvas className="challenge"/>
+					<div className="challenge">
+						<canvas className="challenge" id="challenge"/>
+					</div>
 					<div className="div_answers">
 					{this.renderAnswer(0)}
 					{this.renderAnswer(1)}
@@ -92,10 +153,29 @@ class Match extends React.Component{
 					</div>
 				</div>
 				<div className="progress_bar">
-				{console.log(this.state)}
+					{this.renderTimer()}
 				</div>
 			</div>
 		);
 	}
+}
+document.addEventListener("DOMContentLoaded", canvasFitsParentDOM);
+
+window.addEventListener("resize", canvasFitsParentDOM);
+
+function canvasFitsParentDOM(){
+	var challenge = document.getElementById('challenge');
+ 
+	// prevents unlimited expending canvas size
+	challenge.width = '10px';
+	challenge.height = '10px';
+	setTimeout(300);
+	////////////////////////////////////////////
+
+ 	challenge.style.width = '100%';
+ 	challenge.style.height = '100%';
+	setTimeout(300);
+ 	challenge.width = challenge.offsetWidth - 1;
+ 	challenge.height = challenge.offsetHeight - 1;
 }
 export default Match;
