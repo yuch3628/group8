50f71f2bb3dc83b916e72236d80d960e8aa69db1
moring-swe <moring.swe@gmail.com>
2022-05-31 12:05:25 +0200

    temp

--- client/src/pages/Challenge.js
+++ client/src/pages/Challenge.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./Challenge.css"
+import {FormattedMessage} from "react-intl";
+
 //import ReactDOM from 'react-dom';
 const Challenge = () =>{
     return (
@@ -17,6 +19,7 @@ function Square(props) {
 		id = {props.id}
 		>
 		{props.value+" TEST"}
+		<FormattedMessage id = "lesson3" defaultMessage="Restaurant"/>
 		</button>
 	);
 }
@@ -31,7 +34,7 @@ class Timer extends React.Component{
 
 	render(){
 		return(
-			<canvas className = "timer" height='6px'/>
+			<canvas className = "timer" id = 'timer' height='6px'/>
 			);
 	}
 }
@@ -47,6 +50,8 @@ class Match extends React.Component{
 			userMatches: Array(4).fill([null,null]),
 			gameHeight: 0,
 			buttonYPoints: Array(4).fill(null),
+			totalTime: 60000, // ms
+			remainingTime: 60000, // ms
 		};
 	}
 	
@@ -160,25 +165,130 @@ class Match extends React.Component{
 	}
 }
 document.addEventListener("DOMContentLoaded", canvasFitsParentDOM);
+document.addEventListener("DOMContentLoaded", setFPS);
+//document.addEventListener("DOMContentLoaded", drawTimer);
+
 
 window.addEventListener("resize", canvasFitsParentDOM);
 
 function canvasFitsParentDOM(){
 	const challenge = document.getElementById('challenge');
- 	
+ 	const timer = document.getElementById('timer');
 	//prevents unlimited expending canvas size
 	if(challenge!=null){
 		challenge.width = '10px';
 		challenge.height = '10px';
+		timer.width = '10px'
 		setTimeout(300);
 		////////////////////////////////////////////
 
 	 	challenge.style.width = '100%';
 	 	challenge.style.height = '100%';
+	 	timer.style.height = '100%';
 		setTimeout(300);
 	 	challenge.width = challenge.offsetWidth - 1;
 	 	challenge.height = challenge.offsetHeight - 1;
+	 	timer.width = timer.offsetWidth - 1;
  	}
+ 	redrawTimer(60000, 30000);
 }
+
+var i = 0;
+function setFPS(){
+	var test = 60000;
+	setInterval(draw, 16.6667);
+}
+
+function draw(){
+	i++;
+	var canvas = document.getElementById('challenge');
+	if(canvas != null && canvas.getContext){
+		var ctx = canvas.getContext('2d');
+		ctx.clearRect(0,0,1110,902);
+		ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.fillRect(10 + i, 10 + i, 50, 50);
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(30 + i, 30 + i, 50, 50);
+        redrawTimer(60000, (60000 - (i * 16.6667)));
+	} else {
+		// if not supported...
+		console.log("ERROR!!!");
+	}
+}
+
+function drawLine(){
+	var canvas = document.getElementById('challenge');
+	if(canvas != null && canvas.getContext){
+		var ctx = canvas.getContext('2d');
+		ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(30, 30, 50, 50);
+	} else {
+		// if not supported...
+		console.log("ERROR!!!");
+	}
+}
+
+// function drawTimer(){
+// 	var canvas = document.getElementById('timer');
+// 	if(canvas != null && canvas.getContext){
+// 		var ctx = canvas.getContext('2d');
+// 		var lingrad = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+// 		lingrad.addColorStop(0, '#FFFFFF');
+// 		lingrad.addColorStop(0.7, '#00FF00');
+// 		lingrad.addColorStop(1, '#00FF00');
+//   		ctx.fillStyle = lingrad;
+//   		ctx.fillRect(0, 0, canvas.width, canvas.height);
+// 	} else {
+// 		console.log("ERROR!!!");
+// 	}
+// }
+
+function redrawTimer(totalTime, remainingTime){
+	var canvas = document.getElementById('timer');
+	if(canvas != null && canvas.getContext){
+		var ctx = canvas.getContext('2d');
+		var remaining = remainingTime / totalTime;
+		var lingrad = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+		lingrad.addColorStop(0, '#FF0000'); // red
+		if(1 - remaining < 0.99 && 1 - remaining > 0.01 ){
+		lingrad.addColorStop(1 - remaining - 0.01, '#FF0000');
+		lingrad.addColorStop(1 - remaining, '#FFFFFF');
+		lingrad.addColorStop(1 - remaining + 0.01, '#00FF00');
+		lingrad.addColorStop(1, '#00FF00');
+	}
+  		ctx.fillStyle = lingrad;
+  		ctx.fillRect(0, 0, canvas.width, canvas.height);
+	}
+}
+
+
+
+let param = new URLSearchParams(window.location.search);
+var lessons = ['Supermarket', 'Campus', 'Restaurant', 'Zoo', 'Breakfast', 'Softdrinks']
+var Lesson = [];
+function getData() {
+    fetch('http://localhost:9000/cards/' + lessons[param.get('lesson')-1],{
+        method:'GET',
+        header: new Headers({'Content-Type': 'application/json'})})
+    .then((res) => res.json())
+    .then(data => {
+        data.forEach(
+        number => {
+                     let obj = {title:number.name, content:number.mandarin } ;
+                      console.log(number);
+                      Lesson.push(obj);
+                    });
+    }).catch(e =>{
+        //Error
+    });
+}
+getData();
+
+console.log(Lesson);
+
+
 
 export default Match;
