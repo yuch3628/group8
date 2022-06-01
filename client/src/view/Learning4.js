506e4e47050f13db977ab32cb26186cfdd2a4b93
yuch3628 <sf10126@yahoo.com.tw>
2022-06-01 09:34:01 +0200

    add comments

--- client/src/view/Learning4.js
+++ client/src/view/Learning4.js
@@ -1,7 +1,14 @@
+/*
+  CONTROLLER FUNCTIONS - Learning page
+  --------------------------------
+  contributors:
+    - Yun-Chien (frontend functionality)
+*/
 import React from 'react';
 import Card from '../component/Card';
 import '../style/Learning1.css';
 
+// get data from the backend and save them to Lesson4
 let Lesson4 = [];
 const getData = () => {
     fetch('http://localhost:9000/cards/Zoo',{
@@ -21,6 +28,8 @@ const getData = () => {
     });
 }
 getData();
+
+// Use Card component to show all the data from the backend in Lesson4
 const Learning4= () =>{
     return (
         <div className="card-wrapper">
--- client/src/view/Learning5.js
+++ client/src/view/Learning5.js
@@ -1,7 +1,14 @@
+/*
+  CONTROLLER FUNCTIONS - Learning page
+  --------------------------------
+  contributors:
+    - Yun-Chien (frontend functionality)
+*/
 import React from 'react';
 import Card from '../component/Card';
 import '../style/Learning1.css';
 
+// get data from the backend and save them to Lesson5
 let Lesson5 = [];
 const getData = () => {
     fetch('http://localhost:9000/cards/Breakfast',{
@@ -21,6 +28,8 @@ const getData = () => {
     });
 }
 getData();
+
+// Use Card component to show all the data from the backend in Lesson5
 const Learning5= () =>{
     return (
         <div className="card-wrapper">
